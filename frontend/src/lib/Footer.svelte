a35cd4991a3b6c28d3a8fd8075cd5aee08ce8702
zsbrown97 <zackbrownbandofficial@gmail.com>
2023-04-12 14:42:16 -0500

    progress for Daniel

---
+++ frontend/src/lib/Footer.svelte
@@ -0,0 +1,115 @@
+<script>
+  import facebookSVG from '../icons/facebook.svg';
+  import instagramSVG from '../icons/instagram.svg'
+  import twitterSVG from '../icons/twitter.svg';
+</script>
+
+<div class="footer">
+  <div class="row">
+    <div class="col">
+      <a href="https://lomocomo.org/we-move-app/">
+        <h2>Mobile App</h2>
+      </a>
+      <a href="https://lomocomo.org/about/contact-us/">
+        <h2>Contact Us</h2>
+      </a>
+      
+    </div>
+    <div class="col">
+      <a href="https://lomocomo.org/?ct_template=advocacy"><h2>Advocacy</h2></a>
+      <a href="https://lomocomo.org/advocacy/livable-streets/"><li>Livable Streets</li></a>
+      <a href="https://lomocomo.org/advocacy/transportation-planning/"><li>Transportation Planning</li></a>
+      <a href="https://lomocomo.org/advocacy/grassroots-organizing/"><li>Grassroots Organizing</li></a>
+      <a href="https://lomocomo.org/advocacy/livable-streets/"><li>Livable Streets</li></a>
+      <a href="https://lomocomo.org/advocacy/missourians-for-responsible-transportation/"><li>State-Level Advocacy</li></a>
+
+    </div>
+    <div class="col">
+      <a href="https://lomocomo.org/get-involved/"><h2>Get Involved</h2></a>
+      <a href="https://lomocomo.org/get-involved/membership/"><li>Membership</li></a>
+      <a href="https://lomocomo.org/get-involved/volunteer/"><li>Volunteer</li></a>
+      <a href="https://lomocomo.org/get-involved/sign-up-for-our-newsletter/"><li>Sign up for our newsletter</li></a>
+    </div>
+    <div class="col">
+      <a href="https://lomocomo.org/about/"><h2>About</h2></a>
+      <a href="https://lomocomo.org/about/staff/"><li>Staff</li></a>
+      <a href="https://lomocomo.org/about/board-of-directors/"><li>Board of Directors</li></a>
+      <a href="https://lomocomo.org/about/annual-reports"><li>Annual Reports</li></a>
+      <a href="https://lomocomo.org/about/legal-and-financial/"><li>Legal and Financial</li></a>
+      <a href="https://lomocomo.org/about/the-rebrand-story/"><li>Our Rebrand Story</li></a>
+      <div class="socials">
+        <a class="socialButton" href="https://facebook.com/localmotioncomo">
+          <img src={facebookSVG} alt="facebook" style="scale:4%;" />
+        </a>
+        <a class="socialButton" href="https://www.instagram.com/localmotioncomo/">
+          <img src={instagramSVG} alt="instagram" style="scale:2.5%;" />
+        </a>
+        <a class="socialButton" href="https://twitter.com/localmotioncomo">
+          <img src={twitterSVG} alt="twitter" style="scale:2.5%;" />
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .footer {
+    background-color: #005687; 
+    display: flex;
+    flex-wrap: wrap;
+    height: auto;
+    justify-content: space-evenly;
+    margin-top: 17.5rem;
+    
+  }
+
+  .col {
+    display: flex; 
+    flex-direction: column; 
+    padding-right: 2rem;
+    padding-left: 2rem;
+  }
+
+  .row {
+    display: flex; 
+    flex-direction: row;
+    flex-wrap: wrap; 
+    gap: 3rem;
+  }
+
+  .socials {
+    display: flex;
+    flex-direction: row;
+    padding-top: 1rem;
+  }
+
+  .socialButton {
+    align-items: center;
+    background-color: #f2a900;
+    border-radius: 50%;
+    display: flex;
+    font-size: 1.5em;
+    height: 2rem;
+    justify-content: center;
+    margin-bottom: 0.5rem;
+    margin-right: 0.5rem;
+    width: 2rem;
+  }
+
+  .footer a {
+    text-decoration: none;
+  }
+
+  .footer h2 {
+    color: #F2A900;
+  }
+
+  .footer li {
+    color: white;
+    list-style-type: none;
+  }
+
+  .footer li:hover {
+    color: #F2A900;
+  }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -37,9 +37,7 @@
   import colors from "$lib/colors.json";
   import "$lib/global.css";
 
-  import FacebookSVG from '../icons/facebook.svg';
-  import InstagramSVG from '../icons/instagram.svg';
-  import TwitterSVG from '../icons/twitter.svg';
+  import Footer from "$lib/Footer.svelte";
 
   // Configure the backend path
   axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
@@ -83,7 +81,8 @@
     findReportedIssue = false,
     showFilters = false,
     hasMoreResults = true,
-    showModal = false;
+    showModal = false,
+    showFooter = true;
 
   let backgroundSelector,
     sectionNewReport,
@@ -132,7 +131,10 @@
       resetState();
       reportNewIssueStep6 = false;
     }, 3000);
+    setTimeout(() => (showFooter = true), 4000);
   }
+
+  $: console.log(showFooter)
 
   let visibleDetails = new Set();
 
@@ -603,7 +605,7 @@
                 setTimeout(() => {
                   scrollToSection(-80);
                 }, 10);
-
+                showFooter = false;
                 findReportedIssue = true;
                 if (!filteredIssuesData) await getIssues();
 
@@ -648,6 +650,7 @@
                 scrollToSection(-380);
               }, 10);
               if (!reportNewIssue && !currentStep) {
+                showFooter = false;
                 reportNewIssue = true;
                 reduceBackGroundOpacity = false;
                 currentStep = 1;
@@ -701,7 +704,9 @@
         {/if}
       </div>
     </div>
-
+    {#if !reportNewIssue && !reportNewIssueStep2 && !reportNewIssueStep3 && !reportNewIssueStep4 && !reportNewIssueStep5 && !reportNewIssueStep6 && !findReportedIssue && showFooter}
+      <Footer />
+    {/if}
     <!-- START Report New Issue Flow -->
 
     {#if reportNewIssueStep2}
@@ -1263,6 +1268,7 @@
             </div>
           </div>
         {/if}
+        
 
         <!-- END Report New Issue Flow -->
 
@@ -1538,121 +1544,12 @@
       </div>
       <div id="position-element" style="display: none"></div>
     </div>
-    
+  
   </div>
-  {#if !reportNewIssue && !reportNewIssueStep2 && !reportNewIssueStep3 && !reportNewIssueStep4 && !reportNewIssueStep5 && !findReportedIssue}
-      <div class="footer">
-        <div class="row">
-          <div class="col">
-            <a href="https://lomocomo.org/we-move-app/">
-              <h2>Mobile App</h2>
-            </a>
-            <a href="https://lomocomo.org/about/contact-us/">
-              <h2>Contact Us</h2>
-            </a>
-            
-          </div>
-          <div class="col">
-            <a href="https://lomocomo.org/?ct_template=advocacy"><h2>Advocacy</h2></a>
-            <a href="https://lomocomo.org/advocacy/livable-streets/"><li>Livable Streets</li></a>
-            <a href="https://lomocomo.org/advocacy/transportation-planning/"><li>Transportation Planning</li></a>
-            <a href="https://lomocomo.org/advocacy/grassroots-organizing/"><li>Grassroots Organizing</li></a>
-            <a href="https://lomocomo.org/advocacy/livable-streets/"><li>Livable Streets</li></a>
-            <a href="https://lomocomo.org/advocacy/missourians-for-responsible-transportation/"><li>State-Level Advocacy</li></a>
-      
-          </div>
-          <div class="col">
-            <a href="https://lomocomo.org/get-involved/"><h2>Get Involved</h2></a>
-            <a href="https://lomocomo.org/get-involved/membership/"><li>Membership</li></a>
-            <a href="https://lomocomo.org/get-involved/volunteer/"><li>Volunteer</li></a>
-            <a href="https://lomocomo.org/get-involved/sign-up-for-our-newsletter/"><li>Sign up for our newsletter</li></a>
-          </div>
-          <div class="col">
-            <a href="https://lomocomo.org/about/"><h2>About</h2></a>
-            <a href="https://lomocomo.org/about/staff/"><li>Staff</li></a>
-            <a href="https://lomocomo.org/about/board-of-directors/"><li>Board of Directors</li></a>
-            <a href="https://lomocomo.org/about/annual-reports"><li>Annual Reports</li></a>
-            <a href="https://lomocomo.org/about/legal-and-financial/"><li>Legal and Financial</li></a>
-            <a href="https://lomocomo.org/about/the-rebrand-story/"><li>Our Rebrand Story</li></a>
-            <div class="socials">
-              <a class="socialButton" href="https://facebook.com/localmotioncomo">
-                <img src={FacebookSVG} alt="facebook" style="scale:4%;" />
-              </a>
-              <a class="socialButton" href="https://www.instagram.com/localmotioncomo/">
-                <img src={InstagramSVG} alt="instagram" style="scale:2.5%;" />
-              </a>
-              <a class="socialButton" href="https://twitter.com/localmotioncomo">
-                <img src={TwitterSVG} alt="twitter" style="scale:2.5%;" />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    {/if}
 {/if}
 
 
 <style>
-  /* Footer */
-  .footer {
-    background-color: #005687; 
-    display: flex;
-    flex-wrap: wrap;
-    height: auto;
-    justify-content: space-evenly;
-    
-  }
-
-  .col {
-    display: flex; 
-    flex-direction: column; 
-    padding-right: 2rem;
-    padding-left: 2rem;
-  }
-
-  .row {
-    display: flex; 
-    flex-direction: row;
-    flex-wrap: wrap; 
-    gap: 3rem;
-  }
-
-  .socials {
-    display: flex;
-    flex-direction: row;
-    padding-top: 1rem;
-  }
-
-  .socialButton {
-    align-items: center;
-    background-color: #f2a900;
-    border-radius: 50%;
-    display: flex;
-    font-size: 1.5em;
-    height: 2rem;
-    justify-content: center;
-    margin-bottom: 0.5rem;
-    margin-right: 0.5rem;
-    width: 2rem;
-  }
-
-  .footer a {
-    text-decoration: none;
-  }
-
-  .footer h2 {
-    color: #F2A900;
-  }
-
-  .footer li {
-    color: white;
-    list-style-type: none;
-  }
-
-  .footer li:hover {
-    color: #F2A900;
-  }
-  /* End of Footer */
 
   .white-closeSVG {
     cursor: pointer;
