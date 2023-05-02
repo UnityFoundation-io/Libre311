80ecb43f6a48939d0f3bf90ab74d85101e8c3700
Daniel Bellone <belloned@objectcomputing.com>
2023-05-02 12:39:40 -0700

    Subtype Displayed In Modal, recaptcha UI setup

---
+++ frontend/src/lib/Recaptcha.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let sitekey;
+  let recaptchaElement;
+
+  onMount(() => {
+    window.grecaptcha.render(recaptchaElement, { sitekey });
+  });
+</script>
+
+<div
+  bind:this="{recaptchaElement}"
+  class="g-recaptcha"
+  data-sitekey="{sitekey}"
+></div>
