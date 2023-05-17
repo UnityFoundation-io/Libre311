27679acc2522d573a8d9393b77dd613c8dfc6ed7
montesm <montesm@objectcomputing.com>
2023-05-17 14:29:05 -0500

    Start backend from playwright

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -1,8 +1,8 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		command: 'npm run start',
+		url: 'http://127.0.0.1:3000'
 	},
 	testDir: './tests',
 	reporter: [ ['html', { open: 'never' }] ]
