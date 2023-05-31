59b5a18f18b68fbb98945fcb7934ada5ea199c08
zsbrown97 <zackbrownbandofficial@gmail.com>
2023-05-31 12:18:39 -0500

    redo of config

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -1,15 +1,18 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
-	webServer: {
-		command: 'npm run start:frontend',
-		url: 'http://127.0.0.1:3000',
-    reuseExistingServer: true
-	},
-	testDir: './tests',
-	reporter: [ ['html', { open: 'never', outputFolder: './reports' }] ],
+  webServer: {
+    command: 'npm run build && npm run preview',
+    port: 4173
+  },
+  testDir: './tests',
+  reporter: [ ['html', { open: 'never', outputFolder: './reports' }] ],
   use: {
     video: 'on'
   }
 };
+ 
+ export default config;
 
-export default config;
+// use: {
+//   video: 'on'
+// }
