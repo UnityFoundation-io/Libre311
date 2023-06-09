4bb6da1b820d52c68db37a9d096842edccac8aa0
zsbrown97 <zackbrownbandofficial@gmail.com>
2023-06-09 14:29:46 -0500

    yet another new wacky approach

--- frontend/tests/offlineMode.spec.js
+++ frontend/tests/offlineMode.spec.js
@@ -46,10 +46,8 @@ test('report new issue in offline mode', async ({ page, context }) => {
 
   // Awaits notification that the offline issue has been uploaded
   // await expect(page.getByText('Your pending offline issue was successfully reported.')).toBeVisible({ timeout: 15000 });
-  // await page.route('**/api/requests.json', route => route.fulfill({
-  //   status: 200
-  // }));
-  await page.getByRole('button', { name: 'Close' }).click({ timeout: 50000 });
+  await expect(page.getByRole('button', { name: 'Close' })).toBeVisible({ timeout: 50000 });
+  await page.getByRole('button', { name: 'Close' }).click();
 
   // Verifies content
   await page.getByRole('button', { name: 'search for reported issues Find a Reported Issue' }).click();
