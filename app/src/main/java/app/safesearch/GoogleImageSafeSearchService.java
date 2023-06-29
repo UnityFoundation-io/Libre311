2c8ae4721a78b64d1bd7d983672ead46bbf41e97
montesm <montesm@objectcomputing.com>
2023-06-29 15:34:11 -0500

    Use client lib for safesearch

--- app/src/main/java/app/safesearch/GoogleImageSafeSearchService.java
+++ app/src/main/java/app/safesearch/GoogleImageSafeSearchService.java
@@ -1,49 +1,70 @@
 package app.safesearch;
 
-import io.micronaut.context.annotation.Property;
+import com.google.cloud.vision.v1.*;
+import com.google.protobuf.ByteString;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
 
 @Singleton
 public class GoogleImageSafeSearchService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(GoogleImageSafeSearchService.class);
+
     @Inject
-    SafeSearchClient client;
+    SafeSearchImageClientFactory safeSearchImageClientFactory;
 
-    @Property(name = "wemove.safesearch.key")
-    protected String key;
-
+    // Derived from https://cloud.google.com/vision/docs/detecting-safe-search#vision_safe_search_detection-java
+    // See https://cloud.google.com/vision/docs/before-you-begin
     public boolean imageIsExplicit(String image) {
-        boolean isImageExplicit = true;
+        List<AnnotateImageRequest> requests = new ArrayList<>();
 
+        ByteString imgBytes = null;
         try {
-            Map payload = Map.of(
-                    "requests", List.of(
-                            Map.of(
-                                    "image", Map.of("content", image),
-                                    "features", List.of(Map.of("type", "SAFE_SEARCH_DETECTION"))
-                            )
-                    )
-            );
-            Map map = client.classifyImage(payload, key);
-            Map safeSearchAnnotationsMap = (Map) ((Map) ((List) map.get("responses")).get(0)).get("safeSearchAnnotation");
-            isImageExplicit = safeSearchAnnotationsMap.entrySet().stream()
-                    .filter((Predicate<Map.Entry<String, String>>) filterEntry -> {
-                        String k = filterEntry.getKey();
-                        return k.equals("adult") || k.equals("violence") || k.equals("racy");
-                    })
-                    .anyMatch((Predicate<Map.Entry<String, String>>) matchEntry -> {
-                        String v = matchEntry.getValue();
-                        return v.equals("LIKELY") || v.equals("VERY_LIKELY");
-                    });
-        } catch (Exception e) {
+            imgBytes = ByteString.readFrom(new ByteArrayInputStream(
+                    Base64.getDecoder().decode(image.getBytes("UTF-8"))
+            ));
+        } catch (IOException e) {
+            LOG.error(e.getMessage());
             e.printStackTrace();
+            return true;
         }
 
-        return isImageExplicit;
+        Image img = Image.newBuilder().setContent(imgBytes).build();
+        Feature feat = Feature.newBuilder().setType(Feature.Type.SAFE_SEARCH_DETECTION).build();
+        AnnotateImageRequest request =
+                AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+        requests.add(request);
+
+        try {
+            BatchAnnotateImagesResponse response = safeSearchImageClientFactory.client().batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+
+            for (AnnotateImageResponse res : responses) {
+                if (res.hasError()) {
+                    LOG.error("Error: %s", res.getError().getMessage());
+                    return true;
+                }
+
+                // For full list of available annotations, see http://g.co/cloud/vision/docs
+                SafeSearchAnnotation annotation = res.getSafeSearchAnnotation();
+                return annotation.getAdult() == Likelihood.LIKELY || annotation.getAdult() == Likelihood.VERY_LIKELY ||
+                        annotation.getViolence() == Likelihood.LIKELY || annotation.getViolence() == Likelihood.VERY_LIKELY ||
+                        annotation.getRacy() == Likelihood.LIKELY || annotation.getRacy() == Likelihood.VERY_LIKELY;
+            }
+        } catch (IOException e) {
+            LOG.error(e.getMessage());
+            e.printStackTrace();
+            return true;
+        }
+
+        return true;
     }
 }
