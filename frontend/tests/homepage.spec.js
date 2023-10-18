a86ce7cced8bb54611d797584e760de14ecc7771
zsbrown97 <zackbrownbandofficial@gmail.com>
2023-10-18 10:00:34 -0500

    test reference to wemove removed

--- frontend/tests/homepage.spec.js
+++ frontend/tests/homepage.spec.js
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('homepage features logo', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-  await page.getByRole('img', { name: 'we move logo' }).isVisible();
+  await page.getByRole('img', { name: 'app logo' }).isVisible();
 });
