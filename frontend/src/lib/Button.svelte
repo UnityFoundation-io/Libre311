9dd6fbff1e833ff044e18a74337a7fe1ba2ff033
Josh Hootman <hootmanj@objectcomputing.com>
2023-12-14 11:07:05 -0600

    improve accessibility
    Signed-off-by: Josh Hootman <hootmanj@objectcomputing.com>

---
+++ frontend/src/lib/Button.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let variant = "text";
+</script>
+
+<button class="{variant}-variant" on:click><slot /> </button>
+
+<style>
+  /* css reset for button */
+  .text-variant {
+    background: none repeat scroll 0 0 transparent;
+    border: medium none;
+    border-spacing: 0;
+    color: #26589f;
+    font-family: "PT Sans Narrow", sans-serif;
+    font-size: 16px;
+    font-weight: normal;
+    line-height: 1.42rem;
+    list-style: none outside none;
+    margin: 0;
+    padding: 0;
+    text-align: left;
+    text-decoration: none;
+    text-indent: 0;
+  }
+</style>
---
+++ frontend/src/lib/IconLink.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let href;
+  export let target = "_self";
+</script>
+
+<a {href} {target}><slot /></a>
+
+<style>
+  a:hover {
+    background-color: rgba(0, 0, 0, 0.04);
+  }
+
+  a {
+    display: inline-flex;
+    padding: 8px;
+    border-radius: 50%;
+    overflow: visible;
+    transition: background-color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  }
+</style>
