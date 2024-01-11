a823e223aeeeef5f785d67ae18b53b585d2e0154
zsbrown97 <brownz@unityfoundation.io>
2024-01-11 11:33:23 -0600

    updated playwright tests for osm
    Signed-off-by: zsbrown97 <brownz@unityfoundation.io>

--- frontend/tests/reportNewIssue.spec.js
+++ frontend/tests/reportNewIssue.spec.js
@@ -20,7 +20,7 @@ test("report new issue with image", async ({ page }) => {
   const issuesButton = await page.waitForSelector("#button-report-issue");
   await issuesButton.click();
 
-  const locationInputLocator = await page.getByPlaceholder("Enter the address");
+  const locationInputLocator = await page.getByPlaceholder("Enter address");
   await locationInputLocator.click();
   await locationInputLocator.fill(
     "12140 Woodcrest Executive Dr, Creve Coeur, MO 63141, USA"
@@ -66,7 +66,7 @@ test("report new issue with image", async ({ page }) => {
   // Review information
   await expect(
     page.getByText(
-      "Issue Location: 12140 Woodcrest Executive Dr, Creve Coeur, MO 63141, USA"
+      "Issue Location: 12140, Woodcrest Executive Drive, Creve Coeur, Saint Louis County, Missouri, 63141, United States"
     )
   ).toBeVisible();
   await expect(page.getByText("Issue Type: Bus Stop")).toBeVisible();
@@ -97,14 +97,13 @@ test("report new issue with other issue type", async ({ page }) => {
   await issuesButton.click();
 
   // Enter issue location
-  await page.getByPlaceholder("Enter the address").click();
-  await page
-    .getByPlaceholder("Enter the address")
-    .fill("12140 Woodcrest Executive Drive");
-  await page.getByPlaceholder("Enter the address").press("Enter");
-  await expect(
-    page.getByText("12140 Woodcrest Executive Dr, Creve Coeur, MO 63141, USA")
-  ).toBeVisible();
+  const locationInputLocator = await page.getByPlaceholder("Enter address");
+  await locationInputLocator.click();
+  await locationInputLocator.fill(
+    "12140 Woodcrest Executive Dr, Creve Coeur, MO 63141, USA"
+  );
+  await locationInputLocator.press("Enter");
+
   await page.getByRole("button", { name: "Next" }).click();
 
   // Issue type "other" selected
