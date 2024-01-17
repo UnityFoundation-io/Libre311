6aec15e6e959f43d6de6d199086475ff1864bac1
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-01-17 10:47:09 -0600

    removing back button
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

--- frontend/src/routes/issues/map/[issue_id]/+page.svelte
+++ frontend/src/routes/issues/map/[issue_id]/+page.svelte
@@ -3,12 +3,4 @@
 	import { serviceRequestsResponse } from '../serviceRequestsResponse';	// replace this with the real response
 </script>
 
-<a href="/issues/map">
-	<div class="m-2">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-			<path fill-rule="evenodd" d="M9.53 2.47a.75.75 0 0 1 0 1.06L4.81 8.25H15a6.75 6.75 0 0 1 0 13.5h-3a.75.75 0 0 1 0-1.5h3a5.25 5.25 0 1 0 0-10.5H4.81l4.72 4.72a.75.75 0 1 1-1.06 1.06l-6-6a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
-		</svg>
-	</div>
-</a>
-
 <ServiceRequestDetails serviceRequest={serviceRequestsResponse[1]}/>
