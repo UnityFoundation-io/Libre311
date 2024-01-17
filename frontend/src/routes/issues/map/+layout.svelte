1872a9ffa2e1cf481d96cf28c493c6636396392b
zsbrown97 <brownz@unityfoundation.io>
2024-01-17 09:13:14 -0600

    added leaflet map to new ui
    Signed-off-by: zsbrown97 <brownz@unityfoundation.io>

--- frontend/src/routes/issues/map/+layout.svelte
+++ frontend/src/routes/issues/map/+layout.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
 	import SideBarMainContentLayout from '$lib/components/SideBarMainContentLayout.svelte';
 
+  // Map imports
+  import OpenStreetMap from './OpenStreetMap.svelte';
+  import type { LatLngExpression } from 'leaflet';
+
+  // Initial View for map
+  const initialView: LatLngExpression = [38.79046, -90.48648];
 	// create the issues context / or load issues and set to state value
 </script>
 
 <SideBarMainContentLayout>
 	<slot slot="side-bar" />
-	<div slot="main-content">map goes here</div>
+	<div slot="main-content">
+    <div class="w-full h-screen">
+      <OpenStreetMap view={initialView} zoom={14}>
+      </OpenStreetMap>
+    </div>
+  </div>
 </SideBarMainContentLayout>
