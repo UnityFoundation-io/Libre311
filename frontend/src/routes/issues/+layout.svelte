bea04e8e610bc31e5018c6584a4572eaea0ed7c2
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-01-22 14:42:39 -0600

    removing unneeded code
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

--- frontend/src/routes/issues/+layout.svelte
+++ frontend/src/routes/issues/+layout.svelte
@@ -1,10 +1,5 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import { useLibre311Context } from '$lib/context/Libre311Context';
 	import ServiceRequestsContextProvider from '$lib/context/ServiceRequestsContextProvider.svelte';
-	const linkResolver = useLibre311Context().linkResolver;
-
-	let detailsLink = linkResolver.issuesList($page.url);
 </script>
 
 <ServiceRequestsContextProvider>
