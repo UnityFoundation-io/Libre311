55cd0937c59acc8e498cd44b88c8cbd8f0792208
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-01-24 13:52:46 -0600

    link resolver
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

--- frontend/src/lib/components/MapListToggle.svelte
+++ frontend/src/lib/components/MapListToggle.svelte
@@ -1,13 +1,16 @@
 <script>
 	import { page } from '$app/stores';
+	import { useLibre311Context } from '$lib/context/Libre311Context';
+
+	const linkResolver = useLibre311Context().linkResolver;
 </script>
 
 <div class="toggle">
-	<a href="/issues/map">
+	<a href={linkResolver.issuesMap($page.url)}>
 		<button class="rounded-l-lg" class:selected={$page.route.id == '/issues/map'}> Map </button>
 	</a>
 
-	<a href="/issues/list">
+	<a href={linkResolver.issuesList($page.url)}>
 		<button class="rounded-r-lg" class:selected={$page.route.id == '/issues/list'}>List </button>
 	</a>
 </div>
