295a9cd9809fe9cdee5a4dbfd873e1a3da1d3015
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-01-24 12:54:20 -0600

    more formatting
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

--- frontend/src/lib/components/MapListToggle.svelte
+++ frontend/src/lib/components/MapListToggle.svelte
@@ -1,13 +1,15 @@
-<div class="toggle">
-	
-		<a href="/issues/map"> 
-      <button class="rounded-l-lg"> Map </button>
-    </a>
+<script>
+	import { page } from '$app/stores';
+</script>
 
-	
-		<a href="/issues/list"> 
-      <button class="rounded-r-lg">List </button>
-    </a>
+<div class="toggle">
+	<a href="/issues/map">
+		<button class="rounded-l-lg" class:selected={$page.route.id == '/issues/map'}> Map </button>
+	</a>
+
+	<a href="/issues/list">
+		<button class="rounded-r-lg" class:selected={$page.route.id == '/issues/list'}>List </button>
+	</a>
 </div>
 
 <style>
@@ -17,11 +19,26 @@
 
 	button {
 		padding: 0.5rem 2rem;
-		background-color: hsl(var(--primary));
-		color: hsl(var(--primary-content));
+		background-color: hsl(var(--surface));
+		color: hsl(var(--content));
+		border: 1px solid hsl(var(--primary));
+		position: relative;
 	}
 
-	button:hover {
-		background-color: hsl(var(--primary-hover));
+	button:hover::before {
+		--tw-surface-opacity: 0.05;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		content: '';
+		position: absolute;
+		opacity: var(--tw-surface-opacity);
+		background-color: hsl(var(--hover));
+	}
+
+	.selected {
+		background-color: hsl(var(--primary));
+		color: hsl(var(--primary-content));
 	}
 </style>
