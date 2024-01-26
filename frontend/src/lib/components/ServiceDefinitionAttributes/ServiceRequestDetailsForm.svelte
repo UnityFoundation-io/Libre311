2952e32fb5599d4b4e5f0414af191e68dbe77155
zsbrown97 <brownz@unityfoundation.io>
2024-01-26 16:21:27 -0600

    readded component
    Signed-off-by: zsbrown97 <brownz@unityfoundation.io>

---
+++ frontend/src/lib/components/ServiceDefinitionAttributes/ServiceRequestDetailsForm.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import type { CreateServiceRequestParams } from '$lib/services/Libre311/Libre311';
+
+	// export let params: Partial<CreateServiceRequestParams> = {}
+</script>
+
+<div></div>
