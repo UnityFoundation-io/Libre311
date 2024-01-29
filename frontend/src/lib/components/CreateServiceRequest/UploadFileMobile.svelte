1ee9bfe98de68f1e9a61b9304f19a5203b4cc01c
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-01-29 16:50:15 -0600

    take photo
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

--- frontend/src/lib/components/CreateServiceRequest/UploadFileMobile.svelte
+++ frontend/src/lib/components/CreateServiceRequest/UploadFileMobile.svelte
@@ -6,9 +6,8 @@
 <div class="flex h-full w-full items-center justify-center">
 	<div class="flex-col">
 		<div class="grid grid-rows-2 gap-3">
-			<Button type="primary" on:click={() => {}}>
-				{messages['photo']['take_photo']}
-			</Button>
+			<input type="file" id="actual-btn" accept="image/*" capture="environment" hidden />
+			<label for="actual-btn">{messages['photo']['take_photo']}</label>
 
 			<Button type="primary" on:click={() => {}}>
 				{messages['photo']['camera_roll']}
@@ -24,3 +23,16 @@
 		</div>
 	</div>
 </div>
+
+<style>
+	label {
+		background-color: hsl(var(--primary));
+		color: hsl(var(--primary-content));
+		padding: 0.5rem;
+		font-family: sans-serif;
+		text-align: center;
+		border-radius: 0.3rem;
+		cursor: pointer;
+		margin-top: 1rem;
+	}
+</style>
