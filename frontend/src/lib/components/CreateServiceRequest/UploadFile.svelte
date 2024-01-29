632bebf995fb31cf79f01aaaa5dd5d91301969e8
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-01-29 14:58:38 -0600

    adding mobile breakpoint
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

---
+++ frontend/src/lib/components/CreateServiceRequest/UploadFile.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Breakpoint from '../Breakpoint.svelte';
+	import UploadFileDesktop from './UploadFileDesktop.svelte';
+	import UploadFileMobile from './UploadFileMobile.svelte';
+</script>
+
+<Breakpoint>
+	<UploadFileDesktop slot="is-desktop" />
+	<UploadFileMobile slot="is-mobile" />
+</Breakpoint>
