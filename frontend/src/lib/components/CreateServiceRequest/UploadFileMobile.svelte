6ac66f0ab35b71e0a944bec64303510f49494dec
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-01-29 14:33:54 -0600

    mobile view
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

---
+++ frontend/src/lib/components/CreateServiceRequest/UploadFileMobile.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import messages from '$media/messages.json';
+	import { Button } from 'stwui';
+</script>
+
+<div class="flex h-full w-full items-center justify-center">
+	<div class="flex-col">
+		<div class="grid grid-rows-2 gap-3">
+			<Button type="primary" on:click={() => {}}>
+				{messages['photo']['take_photo']}
+			</Button>
+
+			<Button type="primary" on:click={() => {}}>
+				{messages['photo']['camera_roll']}
+			</Button>
+
+			<Button type="link" on:click={() => {}}>
+				{messages['photo']['no_upload']}
+			</Button>
+
+			<Button type="link" on:click={() => {}}>
+				{messages['photo']['back']}
+			</Button>
+		</div>
+	</div>
+</div>
