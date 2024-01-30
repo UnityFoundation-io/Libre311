af6e06ed693425c276ae7b3a59359d867baa6cf3
Josh Hootman <hootmanj@objectcomputing.com>
2024-01-30 16:23:48 -0600

    update form to meet requirements
    Signed-off-by: Josh Hootman <hootmanj@objectcomputing.com>

--- frontend/src/lib/components/CreateServiceRequest/ServiceRequestDetailsForm.svelte
+++ frontend/src/lib/components/CreateServiceRequest/ServiceRequestDetailsForm.svelte
@@ -1,136 +1,142 @@
+<script lang="ts" context="module"></script>
+
 <script lang="ts">
 	import { useLibre311Service } from '$lib/context/Libre311Context';
-	import type {
-		AttributeResponse,
-		GetServiceListResponse,
-		ServiceCode,
-		ServiceDefinition,
-		ServiceDefinitionAttribute
-	} from '$lib/services/Libre311/Libre311';
-	import {
-		ASYNC_IN_PROGRESS,
-		asAsyncSuccess,
-		type AsyncResult,
-		asAsyncFailure
-	} from '$lib/services/http';
-	import { Select, TextArea } from 'stwui';
-	import type { SelectOption } from 'stwui/types';
-	import { onMount, type ComponentEvents } from 'svelte';
+	import type { CreateServiceRequestParams, Service } from '$lib/services/Libre311/Libre311';
+	import { asAsyncSuccess, type AsyncResult } from '$lib/services/http';
+	import { TextArea } from 'stwui';
+
 	import MultiSelectServiceDefinitionAttribute from './ServiceDefinitionAttributes/MultiSelectServiceDefinitionAttribute.svelte';
 	import DateTimeServiceDefinitionAttribute from './ServiceDefinitionAttributes/DateTimeServiceDefinitionAttribute.svelte';
 	import StringServiceDefinitionAttribute from './ServiceDefinitionAttributes/StringServiceDefinitionAttribute.svelte';
 	import SingleValueListServiceDefinitionAttribute from './ServiceDefinitionAttributes/SingleValueListServiceDefinitionAttribute.svelte';
 	import NumberServiceDefinitionAttribute from './ServiceDefinitionAttributes/NumberServiceDefinitionAttribute.svelte';
 	import TextServiceDefinitionAttribute from './ServiceDefinitionAttributes/TextServiceDefinitionAttribute.svelte';
-	import type { AttributeSelection } from './ServiceDefinitionAttributes/shared';
 
-	// export let params: Readyonly<Partial<CreateServiceRequestParams>> = {};
-	//
+	import SelectARequestCategory from './SelectARequestCategory.svelte';
+	import {
+		createAttributeInputMap,
+		type AttributeInputMap,
+		isMultiSelectServiceDefinitionAttributeInput,
+		isSingleValueListServiceDefinitionAttributeInput,
+		isDateTimeServiceDefinitionAttributeInput,
+		isStringServiceDefinitionInput,
+		isNumberServiceDefinitionInput,
+		isTextServiceDefinitionAttributeInput
+	} from './ServiceDefinitionAttributes/shared';
+	import { createEventDispatcher, type ComponentEvents } from 'svelte';
+	import type { StepChangeEvent } from './types';
+
+	export let params: Partial<CreateServiceRequestParams>;
+
 	const libre311 = useLibre311Service();
+	const dispatch = createEventDispatcher<StepChangeEvent>();
 
-	let serviceList: AsyncResult<GetServiceListResponse> = ASYNC_IN_PROGRESS;
-	let selectedServiceDefinition: AsyncResult<ServiceDefinition> | undefined;
-	let selectedServiceCode: ServiceCode | undefined;
+	let asyncAttributeInputMap: AsyncResult<AttributeInputMap> | undefined;
+	let selectedService: Service | undefined = params.service;
 
-	let attributeResponseMap: Map<
-		ServiceDefinitionAttribute['code'],
-		AttributeResponse[] | AttributeResponse
-	> = new Map();
+	$: if (selectedService) getServiceDefinition(selectedService);
 
-	$: attributeResponses = deriveResponses(attributeResponseMap);
-	$: console.log(attributeResponses);
-	$: if (selectedServiceCode) getServiceDefinition(selectedServiceCode);
-
-	function deriveResponses(
-		attributeResponseMap: Map<
-			ServiceDefinitionAttribute['code'],
-			AttributeResponse[] | AttributeResponse
-		>
-	): AttributeResponse[] {
-		const attributeResponseArr: AttributeResponse[] = [];
-
-		for (const attributeRes of attributeResponseMap.values()) {
-			if (Array.isArray(attributeRes)) {
-				attributeResponseArr.push(...attributeRes);
-			} else attributeResponseArr.push(attributeRes);
-		}
-		return attributeResponseArr;
-	}
-
-	onMount(() => {
-		libre311
-			.getServiceList()
-			.then((res) => (serviceList = asAsyncSuccess(res)))
-			.catch((err) => (serviceList = asAsyncFailure(err)));
-	});
-
-	function updateAttributeResponseMap(e: CustomEvent<AttributeSelection>) {
-		if (!e.detail) return;
-		attributeResponseMap.set(e.detail.code, e.detail.attributeResponse);
-		attributeResponseMap = attributeResponseMap;
-	}
-
-	function createSelectOptions(res: GetServiceListResponse): SelectOption[] {
-		return res.map((s) => ({ value: s.service_code, label: s.service_name }));
-	}
-
-	function issueTypeChange(e: Event) {
-		const target = e.target as HTMLSelectElement;
-		selectedServiceCode = target.value;
-	}
-
-	async function getServiceDefinition(service_code: ServiceCode) {
-		selectedServiceDefinition = undefined;
-		if (
-			serviceList.type === 'success' &&
-			serviceList.value.find((s) => s.service_code == service_code)?.metadata
-		) {
-			const res = await libre311.getServiceDefinition({ service_code });
-			selectedServiceDefinition = asAsyncSuccess(res);
+	async function getServiceDefinition(selectedService: Service) {
+		asyncAttributeInputMap = undefined;
+		if (selectedService.metadata) {
+			const payload = { service_code: selectedService.service_code };
+			const res = await libre311.getServiceDefinition(payload);
+			asyncAttributeInputMap = asAsyncSuccess(createAttributeInputMap(res, params));
+		} else {
+			asyncAttributeInputMap = asAsyncSuccess(new Map());
 		}
 	}
 
-	// 1 getServiceList 									(x)
-	// 2 render the servicedefintionattribute form inputs 	(x)
-	// 3 capture what the user inputs in those inputs 		(x)
-	// 4 validate the inputs
-	// 5 dispatch the event given the inputs are valid
+	function validate() {
+		const errMsg = 'This value is required';
+
+		if (asyncAttributeInputMap?.type === 'success') {
+			let hasError = false;
+			for (const input of asyncAttributeInputMap.value.values()) {
+				if (input.attribute.required) {
+					if (isMultiSelectServiceDefinitionAttributeInput(input)) {
+						if (input.value && input.value.length > 0) input.error = '';
+						else {
+							hasError = true;
+							input.error = errMsg;
+						}
+					} else if (isDateTimeServiceDefinitionAttributeInput(input)) {
+						if (input.value) input.error = '';
+						else {
+							hasError = true;
+							input.error = errMsg;
+						}
+					} else if (isStringServiceDefinitionInput(input)) {
+						if (input.value && input.value.length > 0) input.error = '';
+						else {
+							hasError = true;
+							input.error = errMsg;
+						}
+					} else if (isSingleValueListServiceDefinitionAttributeInput(input)) {
+						if (input.value) input.error = '';
+						else {
+							hasError = true;
+							input.error = errMsg;
+						}
+					} else if (isNumberServiceDefinitionInput(input)) {
+						if (input.value) input.error = '';
+						else {
+							hasError = true;
+							input.error = errMsg;
+						}
+					} else if (isTextServiceDefinitionAttributeInput(input)) {
+						if (input.value && input.value.length > 0) input.error = '';
+						else {
+							hasError = true;
+							input.error = errMsg;
+						}
+					} else {
+						throw Error('Illegal argument, attribute datatype not supported');
+					}
+				} else {
+					input.error = undefined;
+				}
+			}
+			// if no errors, dispatch
+			if (!hasError) {
+				if (!selectedService) throw Error('Service must be selected');
+				const updatedParams: Partial<CreateServiceRequestParams> = {
+					attributeMap: asyncAttributeInputMap.value,
+					service: selectedService
+				};
+				dispatch('stepChange', updatedParams);
+			} else {
+				asyncAttributeInputMap = asAsyncSuccess(asyncAttributeInputMap.value);
+			}
+		}
+	}
+
+	function handleServiceSelected(e: ComponentEvents<SelectARequestCategory>['serviceSelected']) {
+		selectedService = e.detail;
+	}
 </script>
 
-{#if serviceList.type === 'success'}
-	{@const selectOptions = createSelectOptions(serviceList.value)}
-	<Select
-		name="select-1"
-		placeholder="Issue Type"
-		on:change={issueTypeChange}
-		options={selectOptions}
-		class="relative mx-8 my-4"
-	>
-		<Select.Options slot="options">
-			{#each selectOptions as option}
-				<Select.Options.Option {option} />
-			{/each}
-		</Select.Options>
-	</Select>
-{/if}
-
-{#if selectedServiceDefinition?.type == 'success'}
-	{#if selectedServiceDefinition?.type === 'success'}
-		{#each selectedServiceDefinition.value.attributes as attribute}
-			{#if attribute.datatype === 'multivaluelist'}
-				<MultiSelectServiceDefinitionAttribute on:change={updateAttributeResponseMap} {attribute} />
-			{:else if attribute.datatype === 'datetime'}
-				<DateTimeServiceDefinitionAttribute {attribute} />
-			{:else if attribute.datatype == 'string'}
-				<StringServiceDefinitionAttribute {attribute} />
-			{:else if attribute.datatype == 'singlevaluelist'}
-				<SingleValueListServiceDefinitionAttribute {attribute} />
-			{:else if attribute.datatype == 'number'}
-				<NumberServiceDefinitionAttribute {attribute} />
-			{:else if attribute.datatype == 'text'}
-				<TextServiceDefinitionAttribute {attribute} />
+<form>
+	<SelectARequestCategory {params} on:serviceSelected={handleServiceSelected} />
+	{#if asyncAttributeInputMap?.type == 'success'}
+		{#each asyncAttributeInputMap.value.values() as input}
+			{#if isMultiSelectServiceDefinitionAttributeInput(input)}
+				<MultiSelectServiceDefinitionAttribute {input} />
+			{:else if isDateTimeServiceDefinitionAttributeInput(input)}
+				<DateTimeServiceDefinitionAttribute {input} />
+			{:else if isStringServiceDefinitionInput(input)}
+				<StringServiceDefinitionAttribute {input} />
+			{:else if isSingleValueListServiceDefinitionAttributeInput(input)}
+				<SingleValueListServiceDefinitionAttribute {input} />
+			{:else if isNumberServiceDefinitionInput(input)}
+				<NumberServiceDefinitionAttribute {input} />
+			{:else if isTextServiceDefinitionAttributeInput(input)}
+				<TextServiceDefinitionAttribute {input} />
 			{/if}
 		{/each}
+		<TextArea name="comments" placeholder="Description" class="relative mx-8 my-4" />
+
+		<button type="submit" on:click|preventDefault={validate}>Submit</button>
 	{/if}
-{/if}
-<TextArea name="comments" placeholder="Description" class="relative mx-8 my-4" />
+</form>
