2109d26de4a86b99b2a6d14855b8749e8c93dec2
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-01-30 11:04:12 -0600

    consolidating components and passing event
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

--- frontend/src/lib/components/CreateServiceRequest/UploadFile.svelte
+++ frontend/src/lib/components/CreateServiceRequest/UploadFile.svelte
@@ -1,10 +1,97 @@
 <script lang="ts">
+	import type { CreateServiceRequestParams } from '$lib/services/Libre311/Libre311';
+	import { createEventDispatcher } from 'svelte';
 	import Breakpoint from '../Breakpoint.svelte';
-	import UploadFileDesktop from './UploadFileDesktop.svelte';
-	import UploadFileMobile from './UploadFileMobile.svelte';
+	import messages from '$media/messages.json';
+	import { Button } from 'stwui';
+	import { FilePicker } from 'stwui';
+	import { uploadIcon } from '$lib/assets/upload-icon.js';
+	import { useLibre311Service } from '$lib/context/Libre311Context';
+	import type { DropResult } from 'stwui/types';
+
+	export let params: Readonly<Partial<CreateServiceRequestParams>>;
+
+	const dispatch = createEventDispatcher();
+
+	const libre311Service = useLibre311Service();
+
+	let files: FileList;
+
+	$: if (files) {
+		uploadFiles(files);
+	}
+
+	function desktopDropFiles(dropFiles: DropResult) {
+		uploadFiles(dropFiles.accepted);
+	}
+
+	function uploadFiles(files: FileList | File[]) {
+		for (const file of files) {
+			console.log(`${file.name}: ${file.size} bytes`);
+			libre311Service.uploadImage(file);
+		}
+
+		dispatch('stepChange');
+	}
 </script>
 
 <Breakpoint>
-	<UploadFileDesktop slot="is-desktop" />
-	<UploadFileMobile slot="is-mobile" />
+	<div slot="is-desktop" class="flex h-full w-full items-center justify-center">
+		<div class="flex-col">
+			<div class="mb-4">
+				<FilePicker onDrop={desktopDropFiles}>
+					<FilePicker.Icon slot="icon" data={uploadIcon} />
+					<FilePicker.Title slot="title">{messages['photo']['upload']}</FilePicker.Title>
+					<FilePicker.Description slot="description">Drag & Drop your file</FilePicker.Description>
+				</FilePicker>
+			</div>
+
+			<div class="grid grid-rows-2 gap-3">
+				<Button
+					type="primary"
+					on:click={() => {
+						dispatch('stepChange');
+					}}
+				>
+					{messages['photo']['no_upload']}
+				</Button>
+
+				<Button type="link" on:click={() => {}}>
+					{messages['photo']['back']}
+				</Button>
+			</div>
+		</div>
+	</div>
+
+	<div slot="is-mobile" class="flex h-full w-full items-center justify-center">
+		<div class="flex-col">
+			<div class="grid grid-rows-2 gap-3">
+				<input
+					type="file"
+					id="actual-btn"
+					accept="image/*"
+					capture="environment"
+					hidden
+					bind:files
+				/>
+				<label for="actual-btn">{messages['photo']['take_photo']}</label>
+
+				<input type="file" name="photo" id="camera-roll-btn" accept="image/*" hidden bind:files />
+				<label for="camera-roll-btn">{messages['photo']['camera_roll']}</label>
+			</div>
+		</div>
+	</div>
 </Breakpoint>
+
+<style>
+	label {
+		background-color: hsl(var(--primary));
+		color: hsl(var(--primary-content));
+		padding: 0.5rem;
+		font-family: sans-serif;
+		text-align: center;
+		border-radius: 0.3rem;
+		cursor: pointer;
+		margin-top: 1rem;
+	}
+</style>
