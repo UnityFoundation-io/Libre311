9c2345428e6385bc0d0d43f6e5aacc4c527ec53c
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-02-07 13:34:13 -0600

    not needed
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

--- frontend/src/lib/utils/types.ts
+++ frontend/src/lib/utils/types.ts
@@ -1,11 +1,4 @@
-import type { FormInputValue } from "./validation";
-
 export type Maybe<T> = T | undefined | null;
 export type HasId<T> = {
 	id: T;
-};
-
-export type Login = {
-	emailInput: FormInputValue<string | undefined>;
-	passwordInput: FormInputValue<string | undefined>;
 };
