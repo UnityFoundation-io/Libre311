bacf3771d0c08428b35ad0346d0dd26620fe8799
zsbrown97 <brownz@unityfoundation.io>
2024-02-07 10:18:21 -0600

    created menu component and added svgs for menu
    Signed-off-by: zsbrown97 <brownz@unityfoundation.io>

---
+++ frontend/src/lib/components/DrawerMenu.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import { Drawer, Menu, Portal } from 'stwui';
+
+	import mapSVG from '$lib/assets/map.svg';
+	import paintBrushSVG from '$lib/assets/paint-brush.svg';
+	import pencilSVG from '$lib/assets/pencil-square.svg';
+	import tableSVG from '$lib/assets/table-cells.svg';
+
+	export let open: boolean = false;
+
+	function toggleDrawer() {
+		open = !open;
+	}
+
+	// Menu
+	interface MenuItem {
+		key: string;
+		label: string;
+		data: string;
+		href: string;
+	}
+
+	const items: MenuItem[] = [
+		{
+			key: 'table',
+			label: 'Service Request Table',
+			data: tableSVG,
+			href: '#table'
+		},
+		{
+			key: 'map',
+			label: 'Request Map',
+			data: mapSVG,
+			href: '#map'
+		},
+		{
+			key: 'manager',
+			label: 'Service Definition Manager',
+			data: pencilSVG,
+			href: '#manager'
+		},
+		{
+			key: 'editor',
+			label: 'Theme Editor',
+			data: paintBrushSVG,
+			href: '#editor'
+		}
+	];
+</script>
+
+<Portal>
+	{#if open}
+		<Drawer handleClose={toggleDrawer} placement="left">
+			<Drawer.Content slot="content">
+				<Menu>
+					{#each items as item}
+						<Menu.Item key={item.key} label={item.label} href={item.href}>
+							<Menu.Item.Icon slot="icon" data={item.data} />
+						</Menu.Item>
+					{/each}
+				</Menu>
+			</Drawer.Content>
+		</Drawer>
+	{/if}
+</Portal>
---
+++ frontend/src/routes/playground/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import SideBarMainContentLayout from '$lib/components/SideBarMainContentLayout.svelte';
+	import { Button } from 'stwui';
+
+	import DrawerMenu from '$lib/components/DrawerMenu.svelte';
+
+	let isOpen: boolean = false;
+
+	function openDrawer() {
+		isOpen = !isOpen;
+	}
+</script>
+
+<SideBarMainContentLayout>
+	<slot slot="side-bar">
+		<Button type="primary" on:click={openDrawer}>Open Menu</Button>
+
+		<DrawerMenu open={isOpen} />
+	</slot>
+	<div slot="main-content" class="relative flex h-full">map</div>
+</SideBarMainContentLayout>
