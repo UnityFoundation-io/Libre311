dde91d9fa1f648b7ec9a892aaca37a53df207b1a
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-02-09 13:53:49 -0600

    adding drawer
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

--- frontend/src/routes/issues/table/+page.svelte
+++ frontend/src/routes/issues/table/+page.svelte
@@ -1,5 +1,26 @@
 <script lang="ts">
 	import AdminTable from '$lib/components/AdminTable.svelte';
+	import { Button, Drawer, Portal } from 'stwui';
+
+	let drawerLeftOpen = false;
+
+	function openDrawerLeft() {
+		drawerLeftOpen = true;
+	}
+
+	function closeDrawerLeft() {
+		drawerLeftOpen = false;
+	}
 </script>
+
+<Button type="primary" on:click={openDrawerLeft}>Open Left</Button>
+
+<Portal>
+	{#if drawerLeftOpen}
+		<Drawer handleClose={closeDrawerLeft} placement="left">
+			<Drawer.Content slot="content">Drawer Content</Drawer.Content>
+		</Drawer>
+	{/if}
+</Portal>
 
 <AdminTable />
