6ad650b3848fc26b12fb52d8dcf369ef258e163d
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-02-08 21:02:43 -0600

    error page
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

---
+++ frontend/src/lib/components/SomethingWentWrong.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { fade } from 'svelte/transition';
+</script>
+
+<div
+	class="flex h-full w-full items-center justify-center"
+	style="background-color: hsl(var(--primary));height: 100dvh;"
+>
+	<div>
+		<h1 in:fade={{ duration: 2000 }}>
+			{`:( Oh No!`}
+		</h1>
+		<h1 in:fade={{ delay: 1000, duration: 2000 }}>
+			{`Something Went Wrong`}
+		</h1>
+	</div>
+</div>
+
+<style>
+	h1 {
+		font-size: 4.5rem;
+		color: hsl(var(--primary-content));
+		margin: 2%;
+	}
+</style>
