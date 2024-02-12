6c6485be18117c663159cadb9ea1df704326cad2
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-02-12 09:03:22 -0600

    using real data
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

--- frontend/src/routes/issues/table/+page.svelte
+++ frontend/src/routes/issues/table/+page.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
 	import AdminTable from '$lib/components/AdminTable.svelte';
+	import { useLibre311Context } from '$lib/context/Libre311Context';
+
+	import { useServiceRequestsContext } from '$lib/context/ServiceRequestsContext';
+
+	const ctx = useServiceRequestsContext();
+	const serviceRequestsRes = ctx.serviceRequestsResponse;
+
+	const linkResolver = useLibre311Context().linkResolver;
 </script>
 
-<AdminTable />
+{#if $serviceRequestsRes.type === 'success'}
+	<AdminTable serviceRequests={$serviceRequestsRes.value.serviceRequests} />
+{/if}
