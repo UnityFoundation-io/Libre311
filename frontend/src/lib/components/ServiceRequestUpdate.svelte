73cad206021f3b39878ca24c6df77c2e7fbc4277
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-02-12 14:50:14 -0600

    moving update service request to inside of a card
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

--- frontend/src/lib/components/ServiceRequestUpdate.svelte
+++ frontend/src/lib/components/ServiceRequestUpdate.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import messages from '$media/messages.json';
-	import { Badge, Card } from 'stwui';
+	import { Badge, Button, Card } from 'stwui';
 	import type { ServiceRequest } from '$lib/services/Libre311/Libre311';
 	import Flag from '$lib/components/Svg/Flag.svelte';
 	import clockIcon from '$lib/assets/Clock.svg';
@@ -31,75 +31,93 @@
 	$: name = createName(serviceRequest);
 </script>
 
-<Card>
-	<div class="mx-4 my-2">
-		<div class="flow-root">
-			<h2 class="float-left text-base tracking-wide">
-				#{serviceRequest.service_request_id}
-			</h2>
-			<Badge class="float-right text-sm" type={getStatus(serviceRequest)}
-				>{serviceRequest.status}</Badge
-			>
-		</div>
+<div class="flex h-full">
+	<Card class="m-2">
+		<div class="flex h-full w-full flex-col">
+			<div class="m-2 flex-grow">
+				<div class="flow-root">
+					<h2 class="float-left text-base tracking-wide">
+						#{serviceRequest.service_request_id}
+					</h2>
 
-		<p class="my-1 text-sm font-extralight">{toTimeStamp(serviceRequest.requested_datetime)}</p>
+					<Badge class="float-right text-sm" type={getStatus(serviceRequest)}
+						>{serviceRequest.status}
+					</Badge>
+				</div>
 
-		{#if serviceRequest.media_url}
-			<div class="serviceImage" style={`background-image: url('${serviceRequest.media_url}');`} />
-		{/if}
+				<p class="my-1 text-sm font-extralight">{toTimeStamp(serviceRequest.requested_datetime)}</p>
 
-		<div class="mb-2 mt-2 flow-root">
-			<h1 class="float-left text-lg">{serviceRequest.service_name}</h1>
-			<div class="float-right">
-				<Flag />
-			</div>
-		</div>
+				{#if serviceRequest.media_url}
+					<div
+						class="serviceImage"
+						style={`background-image: url('${serviceRequest.media_url}');`}
+					/>
+				{/if}
 
-		<div class="mb-2">
-			<p class="text-sm">{serviceRequest.address}</p>
-		</div>
+				<div class="mb-2 mt-2 flow-root">
+					<h1 class="float-left text-lg">{serviceRequest.service_name}</h1>
+					<div class="float-right">
+						<Flag />
+					</div>
+				</div>
 
-		<div class="mb-1">
-			<strong class="text-base">{messages['serviceRequest']['description']}</strong>
-			<p class="text-sm">{serviceRequest.description}</p>
-		</div>
+				<div class="mb-2">
+					<p class="text-sm">{serviceRequest.address}</p>
+				</div>
 
-		{#if name}
-			<div class="mb-1">
-				<strong class="text-base">{messages['serviceRequest']['citizen_contact']}</strong>
-				<p class="text-sm">{name ?? ''}</p>
-				<p class="text-sm">{serviceRequest.email ?? ''}</p>
-				<p class="text-sm">{serviceRequest.phone ?? ''}</p>
-			</div>
-		{/if}
+				<div class="mb-1">
+					<strong class="text-base">{messages['serviceRequest']['description']}</strong>
+					<p class="text-sm">{serviceRequest.description}</p>
+				</div>
 
-		{#if serviceRequest.agency_responsible}
-			<div class="mb-1">
-				<strong class="text-base">{messages['serviceRequest']['agency_contact']}</strong>
-				<p class="text-sm">{serviceRequest.agency_responsible}</p>
-			</div>
-		{/if}
+				{#if name}
+					<div class="mb-1">
+						<strong class="text-base">{messages['serviceRequest']['citizen_contact']}</strong>
+						<p class="text-sm">{name ?? ''}</p>
+						<p class="text-sm">{serviceRequest.email ?? ''}</p>
+						<p class="text-sm">{serviceRequest.phone ?? ''}</p>
+					</div>
+				{/if}
 
-		<div class="mb-1 flex">
-			<img alt="clock" src={clockIcon} />
-			<strong class="ml-1 text-base">{messages['serviceRequest']['expected_datetime']}</strong>
-			<div class="ml-1 flex items-center">
-				{#if serviceRequest.expected_datetime}
-					<p class="text-sm">{toTimeStamp(serviceRequest.expected_datetime)}</p>
-				{:else}
-					<p class="text-sm">--</p>
+				{#if serviceRequest.agency_responsible}
+					<div class="mb-1">
+						<strong class="text-base">{messages['serviceRequest']['agency_contact']}</strong>
+						<p class="text-sm">{serviceRequest.agency_responsible}</p>
+					</div>
+				{/if}
+
+				<div class="mb-1 flex">
+					<img alt="clock" src={clockIcon} />
+					<strong class="ml-1 text-base">{messages['serviceRequest']['expected_datetime']}</strong>
+					<div class="ml-1 flex items-center">
+						{#if serviceRequest.expected_datetime}
+							<p class="text-sm">{toTimeStamp(serviceRequest.expected_datetime)}</p>
+						{:else}
+							<p class="text-sm">--</p>
+						{/if}
+					</div>
+				</div>
+
+				{#if serviceRequest.status_notes}
+					<div class="mb-1">
+						<h2 class="text-base">{messages['serviceRequest']['service_notes']}</h2>
+						<p class="text-sm">{toTimeStamp(serviceRequest.status_notes)}</p>
+					</div>
 				{/if}
 			</div>
-		</div>
 
-		{#if serviceRequest.status_notes}
-			<div class="mb-1">
-				<h2 class="text-base">{messages['serviceRequest']['service_notes']}</h2>
-				<p class="text-sm">{toTimeStamp(serviceRequest.status_notes)}</p>
+			<div class="m-2 flex items-center justify-between">
+				<Button href="/issues/table">
+					{messages['updateServiceRequest']['button_back']}
+				</Button>
+
+				<Button type="primary" on:click>
+					{messages['updateServiceRequest']['button_submit']}
+				</Button>
 			</div>
-		{/if}
-	</div>
-</Card>
+		</div>
+	</Card>
+</div>
 
 <style>
 	h1 {
