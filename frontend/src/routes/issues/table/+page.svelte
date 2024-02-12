6e6dbb8f813e085e8d66de4f34efe192ccd80697
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-02-12 17:58:08 -0600

    nothing selected card
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

--- frontend/src/routes/issues/table/+page.svelte
+++ frontend/src/routes/issues/table/+page.svelte
@@ -1,1 +1,17 @@
-<h1>Nothing selected</h1>
+<script lang="ts">
+	import { Card } from 'stwui';
+</script>
+
+<div class="flex h-full w-full">
+	<Card class="m-2 w-full" style="background-color: hsl(var(--surface));">
+		<div class="flex h-full w-full items-center justify-center">
+			<h1 class="text-l">Nothing selected</h1>
+		</div>
+	</Card>
+</div>
+
+<style>
+	h1 {
+		color: hsl(var(--primary));
+	}
+</style>
