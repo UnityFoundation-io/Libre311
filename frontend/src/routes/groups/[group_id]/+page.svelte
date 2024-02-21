10be19eab802b30156e906fe967a33e65677e428
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-02-21 13:32:50 -0600

    bread crumb layout
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

---
+++ frontend/src/routes/groups/[group_id]/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Group ID</h1>
+
+<div>List of services</div>
+
+<div>Trail Maintance</div>
+<div>Playground Equipment</div>
+<div>Animal Attack</div>
---
+++ frontend/src/routes/groups/[group_id]/services/[service_id]/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Services</h1>
+
+<div>create attribute</div>
+
+<div>list attributes</div>
+
+<div>edit attribute</div>
+
+<div>delete attribute</div>
