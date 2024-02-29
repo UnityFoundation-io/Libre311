db3a4b1679dcb1d4e693d4d5ef3f249019a1e497
zsbrown97 <brownz@unityfoundation.io>
2024-02-29 10:23:31 -0600

    started working on edit service -e Signed-off-by: zsbrown97 <brownz@unityfoundation.io>

--- frontend/src/routes/groups/[group_id]/+page.svelte
+++ frontend/src/routes/groups/[group_id]/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { Breadcrumbs, Card, List } from 'stwui';
+	import { Breadcrumbs, Button, Card, List, Modal, Portal } from 'stwui';
+
+	import { ellipsisSVG } from '$lib/components/Svg/outline/EllipsisVertical.svelte';
+
+	let modalOpen: boolean = false;
 
 	interface Crumb {
 		label: string;
@@ -26,6 +30,14 @@
 			title: 'Trail Maintenance'
 		}
 	];
+
+	function openModal() {
+		modalOpen = true;
+	}
+
+	function closeModal() {
+		modalOpen = false;
+	}
 </script>
 
 <Card bordered={true} class="m-4">
@@ -41,13 +53,30 @@
 	<Card.Content slot="content" class="p-0 sm:p-0">
 		<List>
 			{#each services as service}
-				<List.Item
-					on:click={() => goto(`/groups/1/services/${service.id}`)}
-					class="cursor-pointer hover:bg-slate-100"
-				>
-					<div class="mx-4">{service.title}</div></List.Item
-				>
+				<List.Item>
+					<List.Item.Content slot="content">
+						<List.Item.Content.Title slot="title">
+							{service.title}
+						</List.Item.Content.Title>
+					</List.Item.Content>
+					<List.Item.Extra slot="extra" placement="start">
+						<Button type="ghost" shape="circle" on:click={openModal}>
+							<Button.Icon data={ellipsisSVG} />
+						</Button>
+					</List.Item.Extra>
+				</List.Item>
 			{/each}
 		</List>
 	</Card.Content>
 </Card>
+
+<Portal>
+	{#if modalOpen}
+		<Modal handleClose={closeModal}>
+			<Modal.Content slot="content">
+				<Modal.Content.Header slot="header">Modal</Modal.Content.Header>
+				<Modal.Content.Body slot="body">I am the content</Modal.Content.Body>
+			</Modal.Content>
+		</Modal>
+	{/if}
+</Portal>
