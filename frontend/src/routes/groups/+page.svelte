1a69b58d2b24f95f95ed9bea18ac8d7fdf5b5d91
zsbrown97 <brownz@unityfoundation.io>
2024-03-01 08:43:04 -0600

    add group button on group page, similar to services page
    Signed-off-by: zsbrown97 <brownz@unityfoundation.io>

--- frontend/src/routes/groups/+page.svelte
+++ frontend/src/routes/groups/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { Breadcrumbs, Card, List } from 'stwui';
+	import { createInput, stringValidator, type FormInputValue } from '$lib/utils/validation';
+	import { Breadcrumbs, Button, Card, Input, List } from 'stwui';
+	import { slide } from 'svelte/transition';
 
 	interface Crumb {
 		label: string;
@@ -19,6 +21,19 @@
 			title: 'Traffic'
 		}
 	];
+
+	let isDropdownVisible = false;
+	let newGroupName: FormInputValue<string> = createInput();
+
+	async function handleAddNewGroup() {
+		newGroupName = stringValidator(newGroupName);
+
+		if (newGroupName.type != 'valid') {
+			return;
+		}
+		newGroupName.value = '';
+		isDropdownVisible = false;
+	}
 </script>
 
 <Card bordered={true} class="m-4">
@@ -30,10 +45,39 @@
 				</Breadcrumbs.Crumb>
 			{/each}
 		</Breadcrumbs>
+		<div class="flex justify-end">
+			<Button
+				type="ghost"
+				on:click={() => {
+					isDropdownVisible = true;
+				}}
+				>{'+ Add Group'}
+			</Button>
+		</div>
 	</Card.Header>
 
 	<Card.Content slot="content" class="p-0 sm:p-0">
 		<List>
+			{#if isDropdownVisible}
+				<div class="m-2 flex" transition:slide|local={{ duration: 500 }}>
+					<Input
+						class="w-[80%]"
+						name="new-service-name"
+						error={newGroupName.error}
+						bind:value={newGroupName.value}
+					></Input>
+
+					<Button
+						class="w-[10%]"
+						on:click={() => {
+							isDropdownVisible = false;
+							newGroupName.value = undefined;
+						}}>Cancel</Button
+					>
+					<Button class="w-[10%]" type="primary" on:click={handleAddNewGroup}>Add</Button>
+				</div>
+			{/if}
+
 			{#each groups as group}
 				<List.Item
 					on:click={() => goto(`/groups/${group.id}`)}
