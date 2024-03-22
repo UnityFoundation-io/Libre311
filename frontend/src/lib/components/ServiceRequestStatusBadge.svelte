eeae1b167e0b4f4ebcaa93786159c37954a1e269
Josh Hootman <hootmanj@objectcomputing.com>
2024-03-22 13:04:05 -0500

    create component for service request status badge
    Signed-off-by: Josh Hootman <hootmanj@objectcomputing.com>

---
+++ frontend/src/lib/components/ServiceRequestStatusBadge.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { ServiceRequestStatus } from '$lib/services/Libre311/Libre311';
+	import { statusToColorMap, statusToTextMap } from '$lib/utils/functions';
+	import { Badge } from 'stwui';
+
+	let clazz: string = '';
+	export { clazz as class };
+	export let status: ServiceRequestStatus;
+</script>
+
+<Badge class={`${clazz} bg-[${statusToColorMap[status]}]`}>{statusToTextMap[status]}</Badge>
