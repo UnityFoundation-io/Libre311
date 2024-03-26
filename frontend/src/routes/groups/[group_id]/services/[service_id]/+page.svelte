5ab8d8b5e7e8dfe172a412624bdb1924ce3580d8
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-03-26 12:00:49 -0500

    displaying attributes
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

--- frontend/src/routes/groups/[group_id]/services/[service_id]/+page.svelte
+++ frontend/src/routes/groups/[group_id]/services/[service_id]/+page.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import { Breadcrumbs, Card, List } from 'stwui';
+	import { Breadcrumbs, Button, Card, Dropdown, List } from 'stwui';
+	import { asAsyncSuccess, type AsyncResult } from '$lib/services/http';
+	import { createAttributeInputMap, type AttributeInputMap } from '$lib/components/CreateServiceRequest/ServiceDefinitionAttributes/shared';
+	import { useLibre311Service } from '$lib/context/Libre311Context';
+	import { page } from '$app/stores';
+
+	const libre311 = useLibre311Service();
+
+	let asyncAttributeInputMap: AsyncResult<AttributeInputMap> | undefined;
+	let selectedServiceCode: string = $page.url.pathname.replace(/\/groups\/(\d+)\/services\//, '');
 
 	interface Crumb {
 		label: string;
@@ -13,24 +21,23 @@
 		{ label: 'Attributes', href: '/groups/1/services/1' }
 	];
 
-	const attributes = [
-		{
-			id: 1,
-			title: 'Something Old'
-		},
-		{
-			id: 2,
-			title: 'Something New'
-		},
-		{
-			id: 3,
-			title: 'Something Borrowed'
-		},
-		{
-			id: 4,
-			title: 'Something Blue'
+	$: updateAttributeMap(selectedServiceCode);
+
+	function updateAttributeMap(service: string) {
+		if (!service) {
+			asyncAttributeInputMap = undefined;
+			return;
 		}
-	];
+		getServiceDefinition(service);
+	}
+
+	async function getServiceDefinition(selectedServiceCode: string) {
+		asyncAttributeInputMap = undefined;
+
+		const payload = { service_code: selectedServiceCode };
+		const res = await libre311.getServiceDefinition(payload);
+		asyncAttributeInputMap = asAsyncSuccess(createAttributeInputMap(res, {}));
+	}
 </script>
 
 <Card bordered={true} class="m-4">
@@ -43,16 +50,20 @@
 			{/each}
 		</Breadcrumbs>
 	</Card.Header>
-	<Card.Content slot="content" class="p-0 sm:p-0">
-		<List>
-			{#each attributes as attribute}
-				<List.Item
-					on:click={() => goto(`/groups/1/services/${attribute.id}`)}
-					class="cursor-pointer hover:bg-slate-100"
-				>
-					<div class="mx-4">{attribute.title}</div></List.Item
-				>
-			{/each}
-		</List>
-	</Card.Content>
+
+	{#if asyncAttributeInputMap?.type == 'success'}
+		<Card.Content slot="content" class="p-0 sm:p-0">
+			<List>
+				{#each asyncAttributeInputMap.value.values() as input}
+					<List.Item class="flex items-center cursor-pointer hover:bg-slate-100">
+
+						<div class="mx-4 w-full">
+							{input.attribute.code}
+						</div>
+
+					</List.Item>
+				{/each}
+			</List>
+		</Card.Content>
+	{/if}
 </Card>
