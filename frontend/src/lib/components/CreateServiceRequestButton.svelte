59437366a6c78cedb50108029cda07cfbce28de4
zsbrown97 <brownz@unityfoundation.io>
2024-03-27 14:29:02 -0500

    added create service request button to map
    Signed-off-by: zsbrown97 <brownz@unityfoundation.io>

---
+++ frontend/src/lib/components/CreateServiceRequestButton.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Button } from 'stwui';
+
+	import PlusCircleIcon from './Svg/outline/PlusCircleIcon.svelte';
+</script>
+
+<div class="absolute bottom-5 left-1/2 z-[1] -translate-x-1/2">
+	<Button type="primary" href={'/issue/create'}>
+		<Button.Leading slot="leading">
+			<PlusCircleIcon slot="icon" />
+		</Button.Leading>
+		Create Service Request
+	</Button>
+</div>
