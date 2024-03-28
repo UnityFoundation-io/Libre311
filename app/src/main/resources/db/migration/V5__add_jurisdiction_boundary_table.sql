53aa17e29ce9fc422812ca33a5dd5ef242017dc7
Josh Hootman <hootmanj@objectcomputing.com>
2024-03-28 14:42:34 -0500

    add on delete cascade
    Signed-off-by: Josh Hootman <hootmanj@objectcomputing.com>

--- app/src/main/resources/db/migration/V5__add_jurisdiction_boundary_table.sql
+++ app/src/main/resources/db/migration/V5__add_jurisdiction_boundary_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS jurisdiction_boundary (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     boundary POLYGON NOT NULL SRID 4326,
     jurisdiction_id VARCHAR(255) NOT NULL,
-    FOREIGN KEY (jurisdiction_id) REFERENCES jurisdictions(id)
+    FOREIGN KEY (jurisdiction_id) REFERENCES jurisdictions(id) ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS boundary_coordinates;
