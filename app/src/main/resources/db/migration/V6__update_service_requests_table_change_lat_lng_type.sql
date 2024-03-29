d1eabf048e23b725e2bd077f257f84daff2e882c
Josh Hootman <hootmanj@objectcomputing.com>
2024-03-29 16:37:24 -0500

    update to actually perform migration for existing service requests
    Signed-off-by: Josh Hootman <hootmanj@objectcomputing.com>

--- app/src/main/resources/db/migration/V6__update_service_requests_table_change_lat_lng_type.sql
+++ app/src/main/resources/db/migration/V6__update_service_requests_table_change_lat_lng_type.sql
@@ -1,9 +1,21 @@
-ALTER TABLE service_requests
-DROP COLUMN latitude;
+CREATE TABLE IF NOT EXISTS temp_service_request_location_table
+AS
+SELECT id, latitude, longitude
+FROM service_requests;
 
 ALTER TABLE service_requests
+DROP COLUMN latitude,
 DROP COLUMN longitude;
 
 ALTER TABLE service_requests
-ADD location POINT NOT NULL SRID 4326;
+ADD location POINT SRID 4326;
 
+UPDATE service_requests AS sr
+    JOIN temp_service_request_location_table AS tr
+ON sr.id = tr.id
+    SET sr.location = ST_SRID(Point(tr.longitude, tr.latitude), 4326);
+
+DROP TABLE IF EXISTS temp_service_request_location_table;
+
+ALTER TABLE service_requests
+MODIFY location POINT NOT NULL SRID 4326;
