9c9519fc59a45cec38c1bca475f4c890a339479b
Josh Hootman <hootmanj@objectcomputing.com>
2024-04-02 14:13:31 -0500

    update map to accept array of control factories
    Signed-off-by: Josh Hootman <hootmanj@objectcomputing.com>

--- frontend/src/lib/components/MapComponent.svelte
+++ frontend/src/lib/components/MapComponent.svelte
@@ -2,11 +2,13 @@
 	export type Events = {
 		boundsChanged: L.LatLngBounds;
 	};
+
+	type ControlFactory = (opts: L.ControlOptions) => L.Control;
 </script>
 
 <script lang="ts">
 	import { onMount, onDestroy, setContext, createEventDispatcher, tick } from 'svelte';
-	import L, { type PointTuple } from 'leaflet';
+	import L from 'leaflet';
 	import 'leaflet/dist/leaflet.css';
 
 	export let locateOpts: L.LocateOptions | undefined = undefined;
@@ -14,6 +16,8 @@
 	export let center: L.LatLngExpression | undefined = undefined;
 	export let zoom: number | undefined = undefined;
 	export let disabled: boolean = false;
+	export let controlFactories: Array<ControlFactory> = [];
+	export let controlOps: L.ControlOptions = { position: 'topleft' };
 
 	const dispatch = createEventDispatcher<Events>();
 
@@ -22,8 +26,14 @@
 
 	onMount(() => {
 		map = L.map(mapElement);
-		map.addEventListener('zoom', () => dispatch('boundsChanged', map.getBounds()));
-		map.addEventListener('drag', () => dispatch('boundsChanged', map.getBounds()));
+		map.addEventListener('moveend', (e) => {
+			dispatch('boundsChanged', map.getBounds());
+		});
+		map.on('locationfound', function (e) {
+			map.setView(e.latlng, 16);
+		});
+
+		controlFactories.forEach((factory) => factory(controlOps).addTo(map));
 
 		L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
 			attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,&copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`
@@ -42,18 +52,18 @@
 
 	function toggleDisabled(map: L.Map | undefined, disabled: boolean) {
 		if (!map) return;
-		const zoomControl = document.querySelector<HTMLElement>('.leaflet-control-zoom');
+		const controlContainer = document.querySelector<HTMLElement>('.leaflet-control-container');
 		if (disabled) {
-			if (zoomControl) {
-				zoomControl.style.display = 'none';
+			if (controlContainer) {
+				controlContainer.style.display = 'none';
 			}
 			map.touchZoom.disable();
 			map.doubleClickZoom.disable();
 			map.scrollWheelZoom.disable();
 			map.dragging.disable();
 		} else {
-			if (zoomControl) {
-				zoomControl.style.display = 'unset';
+			if (controlContainer) {
+				controlContainer.style.display = 'unset';
 			}
 			map.touchZoom.enable();
 			map.doubleClickZoom.enable();
@@ -67,7 +77,9 @@
 			map.fitBounds(bounds);
 		} else if (center && zoom) {
 			map.setView(center, zoom);
-		} else if (locateOpts) map.locate(locateOpts);
+		} else if (locateOpts) {
+			map.locate(locateOpts);
+		}
 	}
 </script>
 
