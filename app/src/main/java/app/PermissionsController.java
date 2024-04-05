e6c9fd2b77a504f47e530e125d1970fa596c3370
Josh Hootman <hootmanj@objectcomputing.com>
2024-04-05 11:09:58 -0500

    format code
    Signed-off-by: Josh Hootman <hootmanj@objectcomputing.com>

--- app/src/main/java/app/PermissionsController.java
+++ app/src/main/java/app/PermissionsController.java
@@ -1,22 +1,13 @@
 package app;
 
-import app.model.jurisdiction.Jurisdiction;
-import app.model.jurisdiction.JurisdictionRepository;
-import app.security.UnityAuthClient;
 import app.security.UnityAuthService;
-import app.security.UnityAuthUserPermissionsRequest;
 import app.security.UserPermissionsResponse;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.micronaut.context.annotation.Property;
-import io.micronaut.core.annotation.Introspected;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
-import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
-import io.micronaut.http.annotation.Post;
 import io.micronaut.http.exceptions.HttpStatusException;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
