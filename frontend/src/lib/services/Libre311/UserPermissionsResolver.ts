96a8961fd19cb00398678447903d7e966ff94a68
Josh Hootman <hootmanj@objectcomputing.com>
2024-04-05 10:09:23 -0500

    add class to retrieve user permissions
    Signed-off-by: Josh Hootman <hootmanj@objectcomputing.com>

---
+++ frontend/src/lib/services/Libre311/UserPermissionsResolver.ts
@@ -0,0 +1,51 @@
+import { z } from 'zod';
+import axios from 'axios';
+import type { UnityAuthLoginResponse } from '../UnityAuth/UnityAuth';
+import { LibrePermissionsSchema } from './Libre311';
+
+export const UserPermissionsSuccessResponseSchema = z.object({
+	permissions: z.array(LibrePermissionsSchema)
+});
+
+export type UserPermissionsSuccessResponse = z.infer<typeof UserPermissionsSuccessResponseSchema>;
+
+export function isUserPermissionsSuccessResponse(
+	unknown: unknown
+): unknown is UserPermissionsSuccessResponse {
+	return UserPermissionsSuccessResponseSchema.safeParse(unknown).success;
+}
+
+export const UserPermissionsFailureResponseSchema = z.object({
+	errorMessage: z.string()
+});
+
+export type UserPermissionsFailureResponse = z.infer<typeof UserPermissionsFailureResponseSchema>;
+
+export const UserPermissionsResponseSchema = z.union([
+	UserPermissionsSuccessResponseSchema,
+	UserPermissionsFailureResponseSchema
+]);
+
+export type UserPermissionsResponse = z.infer<typeof UserPermissionsResponseSchema>;
+
+export type UserPermissionsResolver = {
+	getUserPermissions(loginRes: UnityAuthLoginResponse): Promise<UserPermissionsResponse>;
+};
+
+export class UserPermissionsResolverImpl implements UserPermissionsResolver {
+	constructor(private props: { libreBaseUrl: string; jurisdictionId: string }) {}
+
+	async getUserPermissions(loginRes: UnityAuthLoginResponse): Promise<UserPermissionsResponse> {
+		const res = await axios.get<unknown>(
+			this.props.libreBaseUrl +
+				`/principal/permissions?jurisdiction_id=${this.props.jurisdictionId}`,
+			{
+				headers: {
+					Authorization: `Bearer ${loginRes.access_token}`
+				}
+			}
+		);
+
+		return UserPermissionsResponseSchema.parse(res.data);
+	}
+}
