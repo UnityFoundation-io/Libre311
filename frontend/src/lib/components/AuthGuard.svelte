07a64914e082bcfcb3176e6ea060d55db65b91e4
Josh Hootman <hootmanj@objectcomputing.com>
2024-04-05 10:49:18 -0500

    add is-anonymous option to authguard
    Signed-off-by: Josh Hootman <hootmanj@objectcomputing.com>

--- frontend/src/lib/components/AuthGuard.svelte
+++ frontend/src/lib/components/AuthGuard.svelte
@@ -2,16 +2,22 @@
 	import { useLibre311Context, type UserInfo } from '$lib/context/Libre311Context';
 	import type { LibrePermissions } from '$lib/services/Libre311/Libre311';
 
-	// user must have one of the permissions in the array
-	export let requires: 'is-authenticated' | LibrePermissions[];
+	type RequiresOpts = 'is-authenticated' | 'is-anonymous' | LibrePermissions[];
 
+	// user must have one of the permissions in the array
+	export let requires: RequiresOpts;
 	const user = useLibre311Context().user;
 
 	$: canView = userCanView(requires, $user);
 
-	function userCanView(requires: 'is-authenticated' | LibrePermissions[], user: UserInfo) {
+	function userCanView(requires: RequiresOpts, user: UserInfo) {
+		if (requires == 'is-anonymous') {
+			return !user;
+		}
+		if (requires == 'is-authenticated') {
+			return Boolean(user);
+		}
 		if (!user) return false;
-		if (requires == 'is-authenticated') return true;
 
 		return user.permissions.some((p) => user.permissions.includes(p));
 	}
