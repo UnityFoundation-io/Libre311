87b770368b9ee9a304f98d6ca766da254e83f8f8
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-04-18 11:10:33 -0500

    ignoring unused variables in eslint
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -7,6 +7,10 @@ module.exports = {
 		'plugin:svelte/recommended',
 		'prettier'
 	],
+	root: {
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': ['error']
+	},
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	parserOptions: {
