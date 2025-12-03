060e186c59030da2ceb17f3907fe3c23254f9fcf
Jesse Elliott <elliottj@objectcomputing.com>
2025-12-03 16:00:02 -0500

    Remove dedicated ResponseProcessor
    Signed-off-by: Jesse Elliott <elliottj@objectcomputing.com>

--- app/src/main/java/app/exception/Libre311ExceptionHandler.java
+++ app/src/main/java/app/exception/Libre311ExceptionHandler.java
@@ -1,15 +1,20 @@
 package app.exception;
 
 import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.annotation.NonNull;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MutableHttpResponse;
 import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.hateoas.JsonError;
 import io.micronaut.http.server.exceptions.ExceptionHandler;
 import io.micronaut.http.server.exceptions.response.ErrorContext;
 import io.micronaut.http.server.exceptions.response.ErrorResponseProcessor;
 import jakarta.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
 
 @Produces
 @Singleton
@@ -19,9 +24,9 @@ public class Libre311ExceptionHandler implements
 
     private static final Logger LOG = LoggerFactory.getLogger(Libre311ExceptionHandler.class);
 
-    private final ErrorResponseProcessor<?> responseProcessor;
+    private final ErrorResponseProcessor<JsonError> responseProcessor;
 
-    public Libre311ExceptionHandler(Libre311ErrorResponseProcessor responseProcessor) {
+    public Libre311ExceptionHandler(ErrorResponseProcessor<JsonError> responseProcessor) {
         this.responseProcessor = responseProcessor;
     }
 
@@ -30,9 +35,27 @@ public class Libre311ExceptionHandler implements
         LOG.error(
             String.format("Libre311Exception" + "\nClass: %s" + "\nLogref: %s" + "\nMessage: %s",
                 exception.getClass(), exception.getLogref(), exception.getMessage()));
-
-        return responseProcessor.processResponse(
-            ErrorContext.builder(request).cause(exception).errorMessage(exception.getMessage())
+        exception.printStackTrace();
+        return processResponse(ErrorContext.builder(request)
+                .cause(exception)
+                .errorMessage(exception.getMessage())
                 .build(), HttpResponse.status(exception.getStatus()));
+    }
+
+    private MutableHttpResponse<JsonError> processResponse(@NonNull ErrorContext errorContext,
+                                                           @NonNull MutableHttpResponse<?> baseResponse) {
+        MutableHttpResponse<JsonError> res = responseProcessor.processResponse(errorContext, baseResponse);
+        addLogrefToJsonError(res, errorContext);
+        return res;
+    }
+
+    private void addLogrefToJsonError(MutableHttpResponse<JsonError> res, ErrorContext errorContext){
+        Optional<JsonError> maybeJsonError = res.getBody();
+        Optional<Throwable> maybeRootCause = errorContext.getRootCause();
+
+        if (maybeJsonError.isPresent() && maybeRootCause.isPresent()
+                && maybeRootCause.get() instanceof Libre311BaseException libre311BaseException) {
+            maybeJsonError.get().logref(libre311BaseException.getLogref());
+        }
     }
 }
