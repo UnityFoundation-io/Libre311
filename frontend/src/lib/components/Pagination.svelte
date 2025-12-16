c07d2c5c1c268fe9a2606345a4fc453a3bf89f45
Tim Dalton <daltont@objectcomputing.com>
2025-12-16 14:40:46 -0600

    Libre311 Accessibility Issue #4
    < button and > link to navigate to previous/next set of requests have no aria-label
    
    I did the equivalent with "title". Both narrator and nvda would say the terms plus we get the tooltip.
    
    Signed-off-by: Tim Dalton <daltont@objectcomputing.com>

--- frontend/src/lib/components/Pagination.svelte
+++ frontend/src/lib/components/Pagination.svelte
@@ -28,6 +28,7 @@
 			type={prevPage ? 'text' : 'ghost'}
 			disabled={!prevPage}
 			shape="circle"
+			title="Previous Set"
 			on:click={scrollDispatch}
 		>
 			<ChevronLeft slot="icon" />
@@ -37,6 +38,7 @@
 			type={nextPage ? 'text' : 'ghost'}
 			disabled={!nextPage}
 			shape="circle"
+			title="Next Set"
 			on:click={scrollDispatch}
 		>
 			<ChevronRight slot="icon" />
