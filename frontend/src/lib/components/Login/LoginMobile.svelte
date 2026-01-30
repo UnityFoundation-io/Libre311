d559b35ea12efcc38fa3667ccfff695dbf394c46
Tim Dalton <daltont@objectcomputing.com>
2026-01-30 09:48:01 -0600

    LIB-91 Depressing "Enter" Button Submits Password For Authentication
    When looking at it, I found other stuff that I corrected:
    - Using <form> turns other buttons into "submits" like the clear input "X" on the inputs.
    The toggle password visibility is not focusable nor responds to keyboard enter
    The login failed message at the top needed role="alert" and be forced to repeat itself on retry.
    
    Signed-off-by: Tim Dalton <daltont@objectcomputing.com>

--- frontend/src/lib/components/Login/LoginMobile.svelte
+++ frontend/src/lib/components/Login/LoginMobile.svelte
@@ -1,51 +1,9 @@
-<script lang="ts">
-	import messages from '$media/messages.json';
-	import { rocketLaunch } from '$lib/components/Svg/outline/rocket-launch.js';
-	import { Button, Input } from 'stwui';
-	import { type FormInputValue } from '$lib/utils/validation';
-	import { fade, draw } from 'svelte/transition';
-	import { createEventDispatcher, onMount, tick } from 'svelte';
-	import { dispatchEventFunctionFactory, type EventDispatchTypeMap } from './shared';
-	import { setUpAlertRole } from '$lib/utils/functions';
-
-	const dispatch = createEventDispatcher<EventDispatchTypeMap>();
-
-	export let emailInput: FormInputValue<string | undefined>;
-	export let passwordInput: FormInputValue<string | undefined>;
-	export let errorMessage: string | undefined;
-
-	let visible = false;
-
-	onMount(() => {
-		visible = true;
-	});
-
-	const { onChange, onSubmit, onCancel } = dispatchEventFunctionFactory(dispatch);
-
-	// pass svelte checks
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	const passwordAutocomplete = 'current-password' as any;
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	const emailAutocomplete = 'email' as any;
-
-	let emailRoot: HTMLElement;
-	let passwordRoot: HTMLElement;
-
-	$: setUpAlertRole(emailInput, emailRoot, 'input#email-mobile', 'email-mobile-error');
-	$: setUpAlertRole(passwordInput, passwordRoot, 'input#password-mobile', 'password-mobile-error');
-
-	onMount(() => {
-		tick().then(() => {
-			// tick is done because the browser suggest can be misplaced if otherwise.
-			emailRoot.querySelector('input')?.focus();
-		});
-	});
-</script>
+<script lang="ts" ✂prettier:content✂="CglpbXBvcnQgbWVzc2FnZXMgZnJvbSAnJG1lZGlhL21lc3NhZ2VzLmpzb24nOwoJaW1wb3J0IHsgcm9ja2V0TGF1bmNoIH0gZnJvbSAnJGxpYi9jb21wb25lbnRzL1N2Zy9vdXRsaW5lL3JvY2tldC1sYXVuY2guanMnOwoJaW1wb3J0IHsgQnV0dG9uLCBJbnB1dCB9IGZyb20gJ3N0d3VpJzsKCWltcG9ydCB7IHR5cGUgRm9ybUlucHV0VmFsdWUgfSBmcm9tICckbGliL3V0aWxzL3ZhbGlkYXRpb24nOwoJaW1wb3J0IHsgZmFkZSwgZHJhdyB9IGZyb20gJ3N2ZWx0ZS90cmFuc2l0aW9uJzsKCWltcG9ydCB7IGNyZWF0ZUV2ZW50RGlzcGF0Y2hlciwgb25Nb3VudCwgdGljayB9IGZyb20gJ3N2ZWx0ZSc7CglpbXBvcnQgeyBkaXNwYXRjaEV2ZW50RnVuY3Rpb25GYWN0b3J5LCB0eXBlIEV2ZW50RGlzcGF0Y2hUeXBlTWFwIH0gZnJvbSAnLi9zaGFyZWQnOwoJaW1wb3J0IHsgc2V0VXBBbGVydFJvbGUgfSBmcm9tICckbGliL3V0aWxzL2Z1bmN0aW9ucyc7CgoJY29uc3QgZGlzcGF0Y2ggPSBjcmVhdGVFdmVudERpc3BhdGNoZXI8RXZlbnREaXNwYXRjaFR5cGVNYXA+KCk7CgoJZXhwb3J0IGxldCBlbWFpbElucHV0OiBGb3JtSW5wdXRWYWx1ZTxzdHJpbmcgfCB1bmRlZmluZWQ+OwoJZXhwb3J0IGxldCBwYXNzd29yZElucHV0OiBGb3JtSW5wdXRWYWx1ZTxzdHJpbmcgfCB1bmRlZmluZWQ+OwoJZXhwb3J0IGxldCBlcnJvck1lc3NhZ2U6IHN0cmluZyB8IHVuZGVmaW5lZDsKCglsZXQgdmlzaWJsZSA9IGZhbHNlOwoKCWNvbnN0IHsgb25DaGFuZ2UsIG9uU3VibWl0LCBvbkNhbmNlbCB9ID0gZGlzcGF0Y2hFdmVudEZ1bmN0aW9uRmFjdG9yeShkaXNwYXRjaCk7CgoJZnVuY3Rpb24gcGFzc3dvcmRFbnRlcmVkKGU6IEN1c3RvbUV2ZW50PEtleWJvYXJkRXZlbnQ+KSB7CgkJY29uc3Qga2UgPSBlIGFzIHVua25vd24gYXMgS2V5Ym9hcmRFdmVudDsKCQlpZiAoa2Uua2V5ID09PSAnRW50ZXInKSB7CgkJCWtlLnByZXZlbnREZWZhdWx0KCk7CgkJCWRvU3VibWl0KCk7CgkJfQoJfQoKCWZ1bmN0aW9uIGRvU3VibWl0KCkgewoJCS8vIENsZWFyaW5nIG1lc3NhZ2VzIGFuZCB1cGRhdGluZyB0aGUgRE9NIGVuc3VyZSBzY3JlZW4gcmVhZGVycyByZXBlYXQgZXJyb3JzCgkJZXJyb3JNZXNzYWdlID0gdW5kZWZpbmVkOwoJCWVtYWlsSW5wdXQuZXJyb3IgPSB1bmRlZmluZWQ7CgkJcGFzc3dvcmRJbnB1dC5lcnJvciA9IHVuZGVmaW5lZDsKCgkJdGljaygpLnRoZW4oKCkgPT4gewoJCQlvblN1Ym1pdCgpOwoJCX0pOwoJfQoKCS8vIHBhc3Mgc3ZlbHRlIGNoZWNrcwoJLy8gZXNsaW50LWRpc2FibGUtbmV4dC1saW5lIEB0eXBlc2NyaXB0LWVzbGludC9uby1leHBsaWNpdC1hbnkKCWNvbnN0IHBhc3N3b3JkQXV0b2NvbXBsZXRlID0gJ2N1cnJlbnQtcGFzc3dvcmQnIGFzIGFueTsKCS8vIGVzbGludC1kaXNhYmxlLW5leHQtbGluZSBAdHlwZXNjcmlwdC1lc2xpbnQvbm8tZXhwbGljaXQtYW55Cgljb25zdCBlbWFpbEF1dG9jb21wbGV0ZSA9ICdlbWFpbCcgYXMgYW55OwoKCWxldCBlbWFpbFJvb3Q6IEhUTUxFbGVtZW50OwoJbGV0IHBhc3N3b3JkUm9vdDogSFRNTEVsZW1lbnQ7CgoJJDogc2V0VXBBbGVydFJvbGUoZW1haWxJbnB1dCwgZW1haWxSb290LCAnaW5wdXQjZW1haWwtbW9iaWxlJywgJ2VtYWlsLW1vYmlsZS1lcnJvcicpOwoJJDogc2V0VXBBbGVydFJvbGUocGFzc3dvcmRJbnB1dCwgcGFzc3dvcmRSb290LCAnaW5wdXQjcGFzc3dvcmQtbW9iaWxlJywgJ3Bhc3N3b3JkLW1vYmlsZS1lcnJvcicpOwoKCW9uTW91bnQoKCkgPT4gewoJCXZpc2libGUgPSB0cnVlOwoKCQl0aWNrKCkudGhlbigoKSA9PiB7CgkJCS8vIHRpY2sgaXMgZG9uZSBiZWNhdXNlIHRoZSBicm93c2VyIHN1Z2dlc3QgY2FuIGJlIG1pc3BsYWNlZCBpZiBvdGhlcndpc2UuCgkJCWVtYWlsUm9vdC5xdWVyeVNlbGVjdG9yKCdpbnB1dCcpPy5mb2N1cygpOwoKCQkJY29uc3QgdG9nZ2xlID0gcGFzc3dvcmRSb290LnF1ZXJ5U2VsZWN0b3IoCgkJCQknLnN0d3VpLWlucHV0LXBhc3N3b3JkLXRvZ2dsZS13cmFwcGVyJwoJCQkpIGFzIEhUTUxFbGVtZW50IHwgbnVsbDsKCQkJaWYgKHRvZ2dsZSkgewoJCQkJdG9nZ2xlLnNldEF0dHJpYnV0ZSgndGFiaW5kZXgnLCAnMCcpOwoJCQkJdG9nZ2xlLnNldEF0dHJpYnV0ZSgncm9sZScsICdidXR0b24nKTsKCQkJCXRvZ2dsZS5zZXRBdHRyaWJ1dGUoJ2FyaWEtbGFiZWwnLCAnVG9nZ2xlIHBhc3N3b3JkIHZpc2liaWxpdHknKTsKCgkJCQl0b2dnbGUuYWRkRXZlbnRMaXN0ZW5lcigna2V5ZG93bicsIChlKSA9PiB7CgkJCQkJaWYgKGUua2V5ID09PSAnRW50ZXInIHx8IGUua2V5ID09PSAnICcpIHsKCQkJCQkJZS5wcmV2ZW50RGVmYXVsdCgpOwoJCQkJCQl0b2dnbGUuY2xpY2soKTsKCQkJCQl9CgkJCQl9KTsKCQkJfQoJCX0pOwoJfSk7Cg==">{}</script>
 
 <div class="block items-center justify-center">
 	<div class="w-full flex-col">
 		{#if errorMessage}
-			<div class="flex justify-center bg-red-500 p-2 text-white">
+			<div role="alert" class="flex justify-center bg-red-500 p-2 text-white">
 				<span>{errorMessage}</span>
 			</div>
 		{/if}
@@ -104,6 +62,7 @@
 				error={passwordInput.error}
 				value={passwordInput.value}
 				autocomplete={passwordAutocomplete}
+				on:keydown={passwordEntered}
 				on:change={(e) => onChange(e, 'password')}
 			>
 				<Input.Label slot="label">{messages['login']['password']['label']}</Input.Label>
@@ -111,7 +70,7 @@
 		</div>
 
 		<div class="m-4 flex gap-2">
-			<Button type="primary" on:click={onSubmit}>
+			<Button type="primary" on:click={doSubmit}>
 				{messages['login']['submit']}
 			</Button>
 			<Button type="default" on:click={onCancel}>
@@ -137,13 +96,4 @@
 	</div>
 </div>
 
-<style>
-	a {
-		text-decoration: underline;
-		color: hsl(var(--secondary-content));
-	}
-
-	a:hover {
-		color: hsl(var(--primary));
-	}
-</style>
+<style ✂prettier:content✂="CglhIHsKCQl0ZXh0LWRlY29yYXRpb246IHVuZGVybGluZTsKCQljb2xvcjogaHNsKHZhcigtLXNlY29uZGFyeS1jb250ZW50KSk7Cgl9CgoJYTpob3ZlciB7CgkJY29sb3I6IGhzbCh2YXIoLS1wcmltYXJ5KSk7Cgl9Cg=="></style>
